d2e24dcc1e4fe1861f083f8cbfa3057ffc98c53f
Feross Aboukhadijeh <feross@feross.org>
2014-04-09 22:34:49 -0700

    kill nbsp

--- bin/post-build.js
+++ bin/post-build.js
@@ -4,6 +4,6 @@ var cp = require('child_process')
 var path = require('path')
 
 var rootPath = path.join(__dirname, '..')
-cp.exec('cp -r node_modules/font-awesome chrome/', { cwd: rootPath }, function (err) {
+cp.exec('cp -r node_modules/font-awesome chrome/', { cwd: rootPath }, function (err) {
   if (err) throw err
 })
